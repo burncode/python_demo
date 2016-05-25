f04a59a9d85daaa4461b24159df8bf61cef1b50c
burncode <maohan0118@gmail.com>
2016-05-25 11:54:43 +0800

    my first python

---
+++ log.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+#author:maohan
+#date:2016.5.25
+import logging
+
+logging.debug("this is debug message!")
+logging.info("this is info message!")
+logging.warning('this is warning message')
